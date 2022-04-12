82c94b3cb8c1e4128e959732cff024a7990ba00c
Michal-Dzurik <misko7104@gmail.com>
2022-04-12 18:34:19 +0200

    Added support for Medium an YouTube in autocomplete info

--- app/src/main/java/sk/po/spse/dzurikm/linkorganizer/utils/AutoURLInfoUtil.java
+++ app/src/main/java/sk/po/spse/dzurikm/linkorganizer/utils/AutoURLInfoUtil.java
@@ -13,6 +13,8 @@ import sk.po.spse.dzurikm.linkorganizer.models.Link;
 public class AutoURLInfoUtil {
     private static final String GUITAR_TABS = "tabs.ultimate-guitar.com";
     private static final String SUPER_MUSIC = "supermusic.cz";
+    private static final String MEDIUM = "medium.com";
+    private static final String YOUTUBE = "www.youtube.com";
 
     public static Link getLink(String url, String response) throws URISyntaxException {
         String domain = getDomain(url);
@@ -24,11 +26,66 @@ public class AutoURLInfoUtil {
             case SUPER_MUSIC:
                 link = superMusic(response,url);
                 break;
+            case MEDIUM:
+                link = medium(response,url);
+                break;
+            case YOUTUBE:
+                link = youtube(response,url);
+                break;
             default:
                 break;
         }
 
         return link;
+    }
+
+    private static Link youtube(String response, String url) {
+        Link link = new Link();
+        String name = "",description = "";
+
+        Document doc = Jsoup.parse(response);
+        Element title = doc.getElementsByTag("title").get(0);
+        //Element author = doc.getElementsByClass("iv-branding-context-name").get(0);
+
+
+        link.setName(withoutLastCharacters(title.text(),9));
+        //link.setDescription(author.text());
+
+        return link;
+    }
+
+
+    private static String withoutLastCharacters(String s,int count){
+        StringBuilder newString = new StringBuilder();
+        for (int i = 0; i < s.length() - count; i++) {
+            newString.append(s.charAt(i));
+        }
+
+        return newString.toString().trim();
+    }
+
+    private static Link medium(String response, String url) {
+        Link link = new Link();
+        String name = "",description = "";
+
+        Document doc = Jsoup.parse(response);
+        for (Element input : doc.select("meta")){
+            if (input.attr("property").equals("og:title")){
+                name = input.attr("content");
+                if (!description.equals("")) break;
+            }
+            if (input.attr("name").equals("author")){
+                description = input.attr("content");
+                if (!name.equals("")) break;
+            }
+
+        }
+
+        link.setName(name);
+        link.setDescription(description);
+
+        return link;
+
     }
 
     private static String getDomain(String url) throws URISyntaxException {
